0b354e8fd764e073ca74fef92644fafcad3bd86b
kaamkit <jsanjeev458@gmail.com>
2026-08-19 21:57:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,18 @@
 export const metadata = {
   title: "KaamKit - Free Online Tools",
-  description: "Free online tools for everyday work",
+  description: "Free online tools for everyday work.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="p72ltLP5q30xe0XmIdCUL2fjze1dqFd_Wvh8jqRCi8E"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
